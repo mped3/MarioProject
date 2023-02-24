bc8034d75b7070278bb144d8e49baba8d2ee2bd9
mped3 <122404980+mped3@users.noreply.github.com>
2023-02-24 01:25:00 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let sonic = document.querySelector('.sonic')
-let moveBy = 10;
+let moveBy = 40;
 
 window.addEventListener('load', () =>{
 sonic.style.position = 'absolute';
@@ -12,8 +12,8 @@ window.addEventListener('keyup', (e) =>{
         sonic.style.top = parseInt(sonic.style.top) - moveBy + 'px'; break
     case 'ArrowDown':
         sonic.style.top = parseInt(sonic.style.top) + moveBy + 'px'; break
-    case 'ArrowLeft':
-            sonic.style.left = parseInt(sonic.style.left) - moveBy + 'px'; break
+    // case 'ArrowLeft':
+    //         sonic.style.left = parseInt(sonic.style.left) - moveBy + 'px'; break
     case 'ArrowRight':
                 sonic.style.left = parseInt(sonic.style.left) + moveBy + 'px'; break
    }
